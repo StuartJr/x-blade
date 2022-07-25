a0fdd86f5e4cb9bf994ee8e3c4241dd2f963edad
pavel.svitkovskiy <svitkovskiy@team.picom.ru>
2022-07-25 23:36:10 +0400

    Init

---
+++ main.js
@@ -0,0 +1,13 @@
+import './src/fonts.css';
+import './src/normalize.css';
+import './style.css';
+import './src/button.css';
+import './src/header.css';
+import './src/main.css';
+import './src/advantages.css';
+import './src/video.css';
+import './src/technology.css';
+
+
+import './assets/src/logo.svg';
+import './assets/src/phone.svg';
